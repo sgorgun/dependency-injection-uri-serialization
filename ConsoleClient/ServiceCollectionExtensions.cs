4c09b09238ea9c43aad62230bc6791bba7c7636c
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2022-05-15 23:27:42 +0300

    Add solution skeleton.

---
+++ ConsoleClient/ServiceCollectionExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using DataReceiving;
+using InMemoryReceiver;
+using JsonSerializer.Serialization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serialization;
+using TextFileReceiver;
+using XDomWriter.Serialization;
+using XmlSerializer.Serialization;
+
+namespace ConsoleClient
+{
+    /// <summary>
+    /// Extension methods for service collection.
+    /// </summary>
+    internal static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add northwind services to service collection.
+        /// </summary>
+        /// <param name="services">Service collection.</param>
+        /// <param name="configuration">Configuration.</param>
+        public static IServiceCollection UseExportDataServices(this IServiceCollection services, IConfiguration configuration, string format, string mode)
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            string txtPath = Path.Combine(path, configuration["textFilePath"]) ??
+                             throw new ArgumentNullException("textFilePath");
+            string xmlPath = Path.Combine(path, configuration["xmlFilePath"]) ??
+                             throw new ArgumentNullException("xmlFilePath");
+            string jsonPath = Path.Combine(path, configuration["jsonFilePath"]) ??
+                              throw new ArgumentNullException("jsonFilePath");
+
+            return format switch
+            {
+                "xml" when mode == "inFile" => services
+                    .AddTransient<IDataReceiver>(_ => new TextStreamReceiver(txtPath))
+                    .AddTransient<IDataSerializer<Uri>, XDomTechnology>(_ => new XDomTechnology(xmlPath))
+                    .AddTransient<IDataSerializer<Uri>, XmlSerializerTechnology>(_ => new XmlSerializerTechnology(xmlPath)),
+                "xml" when mode == "inMemory" => services
+                    .AddTransient<IDataReceiver>(_ => new InMemoryDataReceiver())
+                    .AddTransient<IDataSerializer<Uri>, XDomTechnology>(_ => new XDomTechnology(xmlPath)),
+                "json" when mode == "inFile" => services
+                    .AddTransient<IDataReceiver>(_ => new TextStreamReceiver(txtPath))
+                    .AddTransient<IDataSerializer<Uri>, JsonSerializerTechnology>(_ => new JsonSerializerTechnology(jsonPath)),
+                "json" when mode == "inMemory" => services
+                    .AddTransient<IDataReceiver>(_ => new InMemoryDataReceiver())
+                    .AddTransient<IDataSerializer<Uri>, JsonSerializerTechnology>(_ => new JsonSerializerTechnology(jsonPath)),
+                _ => throw new ArgumentException(nameof(format), format, null)
+            };
+        }
+    }
+}
---
+++ ExportDataService.Tests/TestCasesData.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using Conversion;
+using DataReceiving;
+using InMemoryReceiver;
+using JsonSerializer.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Serialization;
+using TextFileReceiver;
+using UriConversion;
+using Validation;
+using XDomWriter.Serialization;
+using XmlDomWriter.Serialization;
+using XmlSerializer.Serialization;
+using XmlWriter.Serialization;
+
+namespace ExportDataService.Tests
+{
+    public class TestCasesData
+    {
+        const string sourceText = "uri-addresses.txt";
+        const string xmlActual = "uri-addresses.xml";
+        const string jsonActual = "uri-addresses.json";
+        const string xmlExpected = "uri-addresses-result.xml";
+        const string jsonExpected = "uri-addresses-result.json";
+
+        public static IEnumerable<TestCaseData> TestCasesForXmlSerializers
+        {
+            get
+            {
+                var serviceCollection = new ServiceCollection()
+                    .AddTransient<IValidator<string>, UriValidator>()
+                    .AddTransient<IConverter<Uri?>, UriConverter>()
+                    .AddTransient<ExportDataService<Uri>>();
+
+                yield return new TestCaseData(serviceCollection
+                    .AddTransient<IDataReceiver>(_ => new TextStreamReceiver(sourceText))
+                    .AddTransient<IDataSerializer<Uri>, XmlSerializerTechnology>(_ =>
+                        new XmlSerializerTechnology(xmlActual)), xmlActual, xmlExpected);
+                yield return new TestCaseData(serviceCollection
+                        .AddTransient<IDataReceiver>(_ => new TextStreamReceiver(sourceText))
+                        .AddTransient<IDataSerializer<Uri>, XDomTechnology>(_ => new XDomTechnology(xmlActual)),
+                    xmlActual,
+                    xmlExpected);
+                yield return new TestCaseData(serviceCollection
+                        .AddTransient<IDataReceiver>(_ => new TextStreamReceiver(sourceText))
+                        .AddTransient<IDataSerializer<Uri>, XmlDomTechnology>(_ => new XmlDomTechnology(xmlActual)),
+                    xmlActual,
+                    xmlExpected);
+                yield return new TestCaseData(serviceCollection
+                        .AddTransient<IDataReceiver>(_ => new TextStreamReceiver(sourceText))
+                        .AddTransient<IDataSerializer<Uri>, XmlWriterTechnology>(
+                            _ => new XmlWriterTechnology(xmlActual)), xmlActual,
+                    xmlExpected);
+                yield return new TestCaseData(serviceCollection
+                    .AddTransient<IDataReceiver>(_ => new InMemoryDataReceiver())
+                    .AddTransient<IDataSerializer<Uri>, XmlSerializerTechnology>(
+                        _ => new XmlSerializerTechnology(xmlActual)), xmlActual, xmlExpected);
+                yield return new TestCaseData(serviceCollection
+                        .AddTransient<IDataReceiver>(_ => new InMemoryDataReceiver())
+                        .AddTransient<IDataSerializer<Uri>, XDomTechnology>(_ => new XDomTechnology(xmlActual)),
+                    xmlActual,
+                    xmlExpected);
+                yield return new TestCaseData(serviceCollection
+                        .AddTransient<IDataReceiver>(_ => new InMemoryDataReceiver())
+                        .AddTransient<IDataSerializer<Uri>, XmlDomTechnology>(_ => new XmlDomTechnology(xmlActual)),
+                    xmlActual,
+                    xmlExpected);
+                yield return new TestCaseData(serviceCollection
+                        .AddTransient<IDataReceiver>(_ => new InMemoryDataReceiver())
+                        .AddTransient<IDataSerializer<Uri>, XmlWriterTechnology>(
+                            _ => new XmlWriterTechnology(xmlActual)), xmlActual,
+                    xmlExpected);
+            }
+        }
+
+        public static IEnumerable<TestCaseData> TestCasesForJsonSerializers
+        {
+            get
+            {
+                var serviceCollection = new ServiceCollection()
+                    .AddTransient<IValidator<string>, UriValidator>()
+                    .AddTransient<IConverter<Uri?>, UriConverter>()
+                    .AddTransient<ExportDataService<Uri>>();
+
+                yield return new TestCaseData(serviceCollection
+                    .AddTransient<IDataReceiver>(_ => new TextStreamReceiver(sourceText))
+                    .AddTransient<IDataSerializer<Uri>, JsonSerializerTechnology>(_ =>
+                        new JsonSerializerTechnology(jsonActual)), jsonActual, jsonExpected);
+                yield return new TestCaseData(serviceCollection
+                    .AddTransient<IDataReceiver>(_ => new InMemoryDataReceiver())
+                    .AddTransient<IDataSerializer<Uri>, JsonSerializerTechnology>(_ =>
+                        new JsonSerializerTechnology(jsonActual)), jsonActual, jsonExpected);
+            }
+        }
+
+        public static IEnumerable<TestCaseData> TestCasesForUriValidator
+        {
+            get
+            {
+                yield return new TestCaseData("https://habrahabr.ru/company/it-grad/blog/341486/", true);
+                yield return new TestCaseData("http://www.example.com/customers/12345", true);
+                yield return new TestCaseData("http://www.example.com/customers/12345/orders/98765", true);
+                yield return new TestCaseData("https://qaevolution.ru/znakomstvo-s-testirovaniem-api/", true);
+                yield return new TestCaseData("http://", false);
+                yield return new TestCaseData("https://www.contoso.com/Home/Index.htm?q1=v1&q2=v2", true);
+                yield return new TestCaseData("http://aaa.com/temp?key=Foo&value=Bar&id=42", true);
+                yield return new TestCaseData("https://www.w3schools.com/html/default.asp", true);
+                yield return new TestCaseData("http://www.ninject.org/learn.html", true);
+                yield return new TestCaseData("https:.php", false);
+                yield return new TestCaseData("https://docs.microsoft.com/ru-ru/dotnet/csharp/programming-guide/concepts/linq/linq-to-xml-overview", true);
+                yield return new TestCaseData("docs.microsoft.com", false);
+                yield return new TestCaseData("microsoft.com/ru-ru/dotnet/csharp/programming-guide/concepts/l", false);
+                yield return new TestCaseData("https://docs.microsoft.com/ru-ru/dotnet/api/system.linq.queryable.where?view=netframework-4.8", true);
+                yield return new TestCaseData("https://docs.microsoft.com/en-us/dotnet/api/system.xml.serialization.xmlserializer?view=net-6.0", true);
+                yield return new TestCaseData("https://metanit.com/python/django/1.1.php", true);
+            }
+        }
+    }
+}
